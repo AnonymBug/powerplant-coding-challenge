d7633e32184c3dc22a4d7adbd6568a1c501e8ec5
Olav <olav.benum@satadsl.net>
2021-04-05 16:45:54 +0200

    Initial

--- service/Controllers/productionplan.cs
+++ service/Controllers/productionplan.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-//using power;
+using power;
 namespace service.Controllers
 {
     public class productionplan : ApiController
     {
+  
 
-
-        public void Post([FromBody] string value)
+        // POST: api/productionplan
+        public void Post([FromBody]string value)
         {
-            string l1 = @"{'load':480,'fuels':{'gas(euro / MWh)':13.4,'kerosine(euro / MWh)':50.8,'co2(euro / ton)':20,'wind(%)':60},'powerplants':[{'name':'gasfiredbig1','type':'gasfired','efficiency':0.53,'pmin':100,'pmax':460},{'name':'gasfiredbig2','type':'gasfired','efficiency':0.53,'pmin':100,'pmax':460},{'name':'gasfiredsomewhatsmaller','type':'gasfired','efficiency':0.37,'pmin':40,'pmax':210},{'name':'tj1','type':'turbojet','efficiency':0.3,'pmin':0,'pmax':16},{'name':'windpark1','type':'windturbine','efficiency':1,'pmin':0,'pmax':150},{'name':'windpark2','type':'windturbine','efficiency':1,'pmin':0,'pmax':36}]}";
-
-            var res = new power.Calculator().Calculate(l1);
+            power.
         }
 
-
+  
     }
 }
